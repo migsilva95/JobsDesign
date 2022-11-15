f60e94cd49550f304bd3a440e95ffa0e2fb4e13d
Miguel Silva <miguelflsilva@hotmail.com>
2022-11-15 10:46:40 +0000

    Last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Grid, Input } from '@mui/material';
+import { Box, Button, Checkbox, Chip, FormControl, Grid, IconButton, ListItemText, MenuItem, Select, SelectChangeEvent } from '@mui/material';
 import d from './data.json';
 import { Opportunities } from './features/opportunities/opportunities';
+import ClearIcon from "@mui/icons-material/Clear";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 function App() {
   
@@ -23,32 +35,154 @@ function App() {
   }
 
   const [data, setData] = useState<MyType[]>([]);
+  const [dataOrdered, setDataOrdered] = useState<MyType[]>([]);
   const [dataToShow, setDataToShow] = useState<MyType[]>([]);
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState<string[]>([]);
+  const [ascending, setAscending] = useState(true);
+  const [descending, setDescending] = useState(true);
+  const [searhString, setSearhString] = useState<string[]>([]);
 
   useEffect(() => {
     setData(d);
-    setDataToShow(d);
+    let obj = [...d].sort((a, b) => {
+      let pA = a.postedAt.split('');
+      let pB = b.postedAt.split('');
+      if ((pB[1] === "d") || (pB[1] === pA[1]) || (pB[1] === "w" && pA[1] === "m")) {
+        if (pA[0] >= pB[0]) {
+          return 1;
+        }
+        else {
+          return -1;
+        }
+      }
+      else {
+        return -1;
+      }
+    });
+    let searchArray: string[] = [];
+    obj.forEach(o => {
+      if(!searchArray.includes(o.role)) {
+        searchArray.push(o.role);
+      }
+      if(!searchArray.includes(o.level)) {
+        searchArray.push(o.level);
+      }
+      o.languages.forEach(l => {
+        if(!searchArray.includes(l)) {
+          searchArray.push(l);
+        }
+      });
+      o.tools.forEach(t => {
+        if(!searchArray.includes(t)) {
+          searchArray.push(t);
+        }
+      });
+    })
+    setSearhString(searchArray);
+    setDataOrdered(obj);
+    setDataToShow(obj);
   }, []);
 
+  useEffect(() => {
+    let obj: MyType[] = [];
+    data.forEach(d => {
+      search.forEach(s => {
+        let pushed = false;
+        if ((d.languages.includes(s) || d.tools.includes(s)) && !pushed) {
+          pushed = true;
+          obj.push(d);
+        }
+      })
+    })
+    if (obj.length === 0) {
+      setDataToShow(dataOrdered);
+    }
+    else {
+      setDataToShow(obj);
+    }
+  }, [data, search]);
+
+  const orderByName = (asc: boolean) => {
+    if (asc) {
+      setAscending(false);
+      setDescending(true);
+      setDataToShow([...data].sort((a, b) => a.position >= b.position ? 1 : -1));
+    }
+    else {
+      setAscending(true);
+      setDescending(false);
+      setDataToShow([...data].sort((a, b) => a.position <= b.position ? 1 : -1));
+    }
+  };
+
+  const resetOrder = () => {
+    setAscending(true);
+    setDescending(true);
+    setDataToShow(dataOrdered);
+  };
+
+  const handleChange = (event: SelectChangeEvent<typeof search>) => {
+    const {
+      target: { value },
+    } = event;
+    setSearch(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Input
-          onChange={(e) => setSearch(e.target.value)}
-          value={search}  />
-          <br />
-          <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-            {dataToShow.map((element) => {
-              return (
-                <>
-                  <Opportunities element={element} />
-                  <br />
-                </>
-              );
-            })}
-          </Grid>
-      </header>
+    <div style={{backgroundColor: 'darksalmon'}}>
+      <div className="App">
+        <header className="App-header">
+          <FormControl sx={{ m: 1}}>
+            <Select
+              multiple
+              value={search}
+              onChange={handleChange}
+              sx={{backgroundColor: 'white'}}
+              renderValue={(selected) => (
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value} />
+                  ))}
+                </Box>
+              )}
+              MenuProps={MenuProps}
+              endAdornment={<IconButton sx={{visibility: search.length > 0 ? "visible" : "hidden"}} onClick={() => setSearch([])}><ClearIcon/></IconButton>}
+            >
+              {searhString.map((name) => (
+                <MenuItem
+                  key={name}
+                  value={name}
+                >
+                  <Checkbox checked={search.indexOf(name) > -1} />
+                  <ListItemText primary={name} />
+                </MenuItem>
+              ))}
+            </Select>
+            <Grid spacing={2}
+              sx={{
+                justifyContent: 'center'
+                }}>
+              {ascending ? <Button variant="outlined" size="small" sx={{backgroundColor: 'white'}} onClick={() => orderByName(true)}>Ascending</Button> : <Button variant="contained" size="small" onClick={() =>  resetOrder()}>Ascending</Button>}
+              {descending ? <Button variant="outlined" size="small" sx={{backgroundColor: 'white'}} onClick={() => orderByName(false)}>Descending</Button> : <Button variant="contained" size="small" onClick={() =>  resetOrder()}>Descending</Button>}
+            </Grid>
+          </FormControl>
+        </header>
+        <body className="App-body">
+            <Grid >
+              {dataToShow.map((element) => {
+                return (
+                  <>
+                    <Opportunities element={element} />
+                    <br />
+                  </>
+                );
+              })}
+            </Grid>
+        </body>
+      </div>
     </div>
   );
 }
--- src/features/opportunities/opportunities.tsx
+++ src/features/opportunities/opportunities.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import { Grid, Card, Typography, CardMedia, CardHeader, Box } from '@mui/material';
+import { Grid, Card, Typography, CardMedia, CardHeader, Box, Chip, Button } from '@mui/material';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import { color } from '@mui/system';
   
 type MyType = {
   id: number,
@@ -20,42 +20,73 @@ type MyType = {
 }
 
 export function Opportunities(props: { element: MyType; }) {
-  const dispatch = useAppDispatch();
   
   return (
     <>
-      <Grid item xs={12} sm={6} md={4} lg={3}>
-        <Card sx={{ maxWidth: 345 }}>
+      <Grid item>
+        <Card>
           <CardMedia
             component="img"
             image={"../." + props.element.logo}
           />
           <Grid container alignItems="center">
             <Grid item>
-              <Typography variant="body2" color="grey">
+              <Typography variant="body1" color="grey">
                 {props.element.company}
               </Typography>
             </Grid>
             {props.element.new &&
-              <Grid item>
-                <Typography variant="body2" color="green">
-                  &nbsp;NEW!
-                </Typography>
-              </Grid>
+              <>
+                <Grid item>
+                  <Typography variant="body1" color="grey">
+                    &nbsp;
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Chip key='NEW!' label='NEW!' style={{ backgroundColor: 'green', color: 'white' }}/>
+                </Grid>
+              </>
             }
             {props.element.featured &&
-              <Grid item>
-                <Typography variant="body2" color="black">
-                  &nbsp;FEATURED
-                </Typography>
-              </Grid>
+              <>
+                <Grid item>
+                  <Typography variant="body1" color="grey">
+                    &nbsp;
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Chip key='FEATURED' label='FEATURED' style={{ backgroundColor: 'black', color: 'white' }}/>
+                </Grid>
+              </>
             }
           </Grid>
-          <Grid container alignItems="center">
-            <Grid item>
-              <Typography variant="h5" color="inherit">
+          <Grid container>
+            <Grid item style={{ flexGrow: "1" }}>
+              <Typography variant="h4" color="inherit" >
                 {props.element.position}
               </Typography>
+            </Grid>
+            <Grid xs={5} item>
+              <Button variant="contained" size="small" sx={{backgroundColor: 'green'}}>
+                {props.element.role}
+              </Button>
+              <Button variant="contained" size="small" sx={{backgroundColor: 'green'}}>
+                {props.element.level}
+              </Button>
+              {props.element.languages.map(l => {
+                return (
+                  <Button variant="contained" size="small" sx={{backgroundColor: 'green'}}>
+                  {l}
+                  </Button>
+                )
+              })}
+              {props.element.tools.map(t => {
+                return (
+                  <Button variant="contained" size="small" sx={{backgroundColor: 'green'}}>
+                  {t}
+                  </Button>
+                )
+              })}
             </Grid>
           </Grid>
           <Grid container alignItems="center">
